2e2b7aecbfd3d4d08e7ddaa250064ec656d8c8b8
05-1951060517-NguyenQuynhAnh <qanh1662001@gmail.com>
2022-03-08 16:25:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/style.css">
+    <title>CSE481 - Tiệm thời trang</title>
+    <link rel="shortcut icon" href="img/logo.png">
+</head>
+
+<body>
+    <div class="header">
+        <div class="container-fluid" style="background:#e68e12;">
+            <div class="container content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col py-3" style="font-size:13px;">
+                            CSE481@GMAIL.COM | Hotline: 0837 473 645
+                        </div>
+                        <div class="col text-nowrap my-auto" style="font-size:13px;">
+                            <p class="mb-0">
+                                Tặng nay voucher trị giá 150.000VNĐ khi mua sản phẩm từ 1.000.000 VNĐ
+                            </p>
+                            <div class="text-center mx-auto">
+                                Đơn hàng được FreeShip khi mua từ 500.000 VNĐ
+                            </div>
+                        </div>
+                        <div class="col d-flex justify-content-end">
+                            <div class="">
+                                <form action="" method="GET">
+                                    <div class="pt-2 ms-5 d-flex">
+                                        <input class="py-2 rounded-start" type="text"
+                                            style="border:none;padding-left: 10px;font-size:12px;width:250px"
+                                            name="namesearch" placeholder="The clothes you need...">
+                                        <button type="submit" class="border-0 rounded-0 rounded-end bg-white"> <svg
+                                                xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z" />
+                                                <path
+                                                    d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z" />
+                                            </svg></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="row mt-2">
+
+                        <img src="img/icon.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="px-4 row">
+                        <p class="px-2 blockquote-footer text-center">“Đừng chạy theo xu hướng. Đừng khiến bản thân lệ thuộc vào thời trang. Hãy để chính mình là người quyết định bản thân sẽ mặc gì cũng như sẽ sống ra sao”.</p>
+                    <p class="px-2 blockquote-footer d-flex justify-content-end">Versace</p>
+                    </div>
+                </div>
+                <div class="col-md ps-0 mt-4">
+                    <div class="row">
+                        <div class="col-md">
+                            <ul class="nav justify-content-center">
+                                <li class="nav-item fw-bold ">
+                                    <a class="nav-link link-dark pe-1" style="font-size:15px" aria-current="page"
+                                        href="#">TRANG CHỦ</a>
+                                </li>
+                                <li class="nav-item fw-bold">
+                                    <a style="font-size:15px" class="pe-1 nav-link dropdown-toggle link-dark" href="#"
+                                        id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
+                                        SẢN PHẨM
+                                    </a>
+                                    <ul
+                                        class="dropdown-menu rounded-0 border-warning border-end-0 border-bottom-0 border-start-0 border-2">
+                                        <li><a class="dropdown-item" STYLE="font-size:13px" href="#">Shop All </a></li>
+                                        <li><a class="dropdown-item" STYLE="font-size:13px" href="#">TOP</a></li>
+                                        <li><a class="dropdown-item" STYLE="font-size:13px" href="#">BOTTOM</a></li>
+                                        <li><a class="dropdown-item" STYLE="font-size:13px" href="#">ACCESSORIES</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item fw-bold">
+                                    <a class=" pe-1 nav-link link-dark" style="font-size:15px" href="#">HƯỚNG DẪN</a>
+                                </li>
+                                <li class="nav-item fw-bold">
+                                    <a class="pe-1 nav-link link-dark" style="font-size:15px">BẢNG SIZE</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3 mt-1 d-flex">
+                            <a href="" class="text-decoration-none link-dark">
+                                <div class="d-flex">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                                        class="bi bi-people me-2" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+                                    </svg>
+                                    <p class="pt-1" style="font-size:11px">Đăng nhập</p>
+                                </div>
+                            </a>
+                            <a href="" class="text-decoration-none link-dark">
+                                <div class="ms-3">
+                                    <p class="pt-1 fw-bold link-primary" style="font-size:11px">Đăng ký</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mt-4 pe-4">
+                        <img src="img/bia.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row px-5 mt-4">
+                <div class="col">
+                    <a href="">
+                        <img class="rounded-3"src="img/top.png" alt="">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="">
+                        <img class="rounded-3"src="img/seller.png" alt="">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="">
+                        <img class="rounded-3"src="img/bottom.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <hr>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
