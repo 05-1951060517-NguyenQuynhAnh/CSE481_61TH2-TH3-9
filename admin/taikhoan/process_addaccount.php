2dc73b194c7382c97cd54642007ccaf44991f241
19-1951060653-NguyenThuyDuong <94334465+19-1951060653-NguyenThuyDuong@users.noreply.github.com>
2022-03-25 16:28:52 +0700

    update taikhoan

--- admin/taikhoan/process_addaccount.php
+++ admin/taikhoan/process_addaccount.php
@@ -39,7 +39,7 @@ $id1 = $_GET['id'];
     $error = "Tài khoản Email đã tồn tại";
     header("location:addaccount.php?id=$id1&error=$error"); 
     }else{
-    $sql = "INSERT INTO account (id,hoten,gioitinh,ngaysinh,email,password) VALUES('$id','$hovaten','$gioitinh','$ngaysinh','$email','$matkhau')";
+    $sql = "INSERT INTO taikhoan (id,hoten,gioitinh,ngaysinh,email,password) VALUES('$id','$hovaten','$gioitinh','$ngaysinh','$email','$matkhau')";
     $result = mysqli_query($conn,$sql);
     if($result ==true){
         header("location:taikhoan.php?id=$id1"); 
